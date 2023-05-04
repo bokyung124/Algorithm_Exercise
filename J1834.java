ae7010e0e4abcbba15af2480aa0469157dff90c8
이보경 <leebk1124@naver.com>
2023-05-04 10:06:10 +0900

    1834

--- J1834.java
+++ J1834.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class J1834 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long num = sc.nextInt();
+        long n = sc.nextInt();
 
-        long sum = (num - 1) * (num + 1 + (num + 1) * (num - 1)) / 2;
+        long sum = (n - 1) * (n + 1 + (n + 1) * (n - 1)) / 2;
 
         System.out.println(sum);
     }
