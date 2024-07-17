1e258a56e14eaf9bf8d46cd222465f2ee5cdf9e0
Bokyung Lee <leebk1124@gmail.com>
2024-07-17 21:19:47 +0900

    [Silver II] Title: 나무 자르기, Time: 2052 ms, Memory: 150200 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,29 +1,32 @@
-n, m = map(int, input().split())
-ls = list(map(int, input().split()))
+from sys import stdin, stdout
 
-# 구해야 할 것: 최소 M미터의 나무를 가져가기 위해 설정해야 할 높이의 최댓값
-# M: 원소 값에서 절단기 높이를 뺀 값들의 합
-# 주어진 배열을 탐색하는게 아니라 자연수 배열을 탐색해야 함
-# mid: 절단기 높이
-
-lo = 0
-hi = max(ls)
-
-answer = 0
-
-while lo <= hi:
-    mid = (lo + hi) // 2
-    s = 0
-    diff = 0
+def main():
+    n, m = map(int, stdin.readline().split())
+    ls = list(map(int, stdin.readline().split()))
     
-    for i in ls:   
-        diff = i - mid
-        if diff > 0:
-            s += diff
-    if s >= m:
-        answer = mid
-        lo = mid + 1
-    else:
-        hi = mid - 1
+    # 구해야 할 것: 최소 M미터의 나무를 가져가기 위해 설정해야 할 높이의 최댓값
+    # M: 원소 값에서 절단기 높이를 뺀 값들의 합
+    # 주어진 배열을 탐색하는게 아니라 자연수 배열을 탐색해야 함
+    # mid: 절단기 높이
+    
+    lo = 0
+    hi = max(ls)
+    
+    answer = 0
+    
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        s = 0
+        diff = 0
+        
+        s += sum(i - mid for i in ls if i > mid)
+        
+        if s >= m:
+            answer = mid
+            lo = mid + 1
+        else:
+            hi = mid - 1
 
-print(answer)
+    print(answer)
+
+main()
