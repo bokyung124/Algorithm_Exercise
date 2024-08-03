9f90d6f2f5d8f9a62994bf57542002a6b45e4fb1
Bokyung Lee <leebk1124@gmail.com>
2024-08-03 18:01:02 +0900

    Time: 212 ms (73.31%), Space: 26.9 MB (77.13%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,28 @@
+"""
+grid: List[List[str]]
+numIslands -> int
+"""
+
+class Solution(object):
+
+    def numIslands(self, grid):
+        def dfs(i,j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+                return
+
+            # 방문한 곳
+            grid[i][j] =0
+
+            # 연결된 1에 대해 dfs 수행
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(i,j)
+                    count +=1
+        return count
