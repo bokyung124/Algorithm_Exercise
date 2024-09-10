04a73e1e4572901d6f46b86dded7b405e19594a4
Bokyung Lee <leebk1124@gmail.com>
2024-09-10 20:40:32 +0900

    [level 4] Title: 사칙연산, Time: 98.55 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.py
@@ -0,0 +1,53 @@
+"""
+dp[i][j]: i~j의 최댓값 -> dp
+dp_min[i][j]: i~j의 최솟값 -> 직접 (모두 더한 뒤 뺌)
+"""
+
+import operator
+
+def solution(arr):
+    operation_map = [
+        "+",
+        "-"
+    ]
+    arr = list(map(lambda x: int(x) if x not in operation_map else x, arr))
+    
+    n = len(arr)
+    INF = 1_000_000
+    
+    dp_max = [[-INF] * n for _ in range(n)]
+    dp_min = [[INF] * n for _ in range(n)]
+    
+    for i in range(0, n + 1, 2):
+        dp_max[i][i] = arr[i]
+        dp_min[i][i] = arr[i]
+    
+    def dp(start, end, is_max=True):
+        if is_max:
+            if dp_max[start][end] != -INF:
+                return dp_max[start][end]
+            ans = -INF
+            for mid in range(start, end, 2):
+                if arr[mid + 1] == "+":
+                    ans = max(ans, (dp(start, mid) + dp(mid + 2, end)))
+                else:
+                    ans = max(ans, (dp(start, mid) - dp(mid + 2, end, False)))
+            dp_max[start][end] = ans
+            return ans
+        else:
+            if dp_min[start][end] != INF:
+                return dp_min[start][end]
+            stack = []
+            cur = arr[start]
+            for idx in range(start, end, 2):
+                if arr[idx + 1] == "+":
+                    cur += arr[idx + 2]
+                else:
+                    stack.append(cur)
+                    cur = arr[idx + 2]
+            stack.append(cur)
+            ans = 2 * stack[0] - sum(stack)
+            dp_min[start][end] = ans
+            return ans
+            
+    return dp(0, n - 1)
